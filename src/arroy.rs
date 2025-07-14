279b5826aac7ecfbfb5e614378ea99e5f78623fc
Tamo <tamo@meilisearch.com>
2025-07-14 10:56:26 +0200

    fmt

--- src/arroy.rs
+++ src/arroy.rs
@@ -5,7 +5,7 @@ use serde::{Deserialize, Serialize};
 
 use crate::{centered_scrollable, Blog};
 
-macros::create_file!(arroy/index.html);
+macros::create_file!(arroy / index.html);
 
 impl Blog {
     pub fn display_arroy_article(&mut self, ctx: &Context) {
